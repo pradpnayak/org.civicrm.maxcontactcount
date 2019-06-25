bda6f3757e2660a07528baa1fde34c5f20cbe3b1
Pradeep Nayak <pradpnayak@gmail.com>
2019-06-25 23:04:38 +0100

    first commit

---
+++ maxcontactcount.php
@@ -0,0 +1,135 @@
+<?php
+
+require_once 'maxcontactcount.civix.php';
+use CRM_Maxcontactcount_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function maxcontactcount_civicrm_config(&$config) {
+  _maxcontactcount_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function maxcontactcount_civicrm_xmlMenu(&$files) {
+  _maxcontactcount_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function maxcontactcount_civicrm_install() {
+  _maxcontactcount_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function maxcontactcount_civicrm_postInstall() {
+  _maxcontactcount_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function maxcontactcount_civicrm_uninstall() {
+  _maxcontactcount_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function maxcontactcount_civicrm_enable() {
+  _maxcontactcount_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function maxcontactcount_civicrm_disable() {
+  _maxcontactcount_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function maxcontactcount_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _maxcontactcount_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function maxcontactcount_civicrm_managed(&$entities) {
+  _maxcontactcount_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function maxcontactcount_civicrm_caseTypes(&$caseTypes) {
+  _maxcontactcount_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function maxcontactcount_civicrm_angularModules(&$angularModules) {
+  _maxcontactcount_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function maxcontactcount_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _maxcontactcount_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
+ */
+function maxcontactcount_civicrm_entityTypes(&$entityTypes) {
+  _maxcontactcount_civix_civicrm_entityTypes($entityTypes);
+}
